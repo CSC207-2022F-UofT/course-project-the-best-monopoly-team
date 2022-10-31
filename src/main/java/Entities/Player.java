c67c6b81e383783d1fe3c8b2690b6922dddb3cd7
giaphuongd <giaphuongd@gmail.com>
2022-10-31 12:45:17 -0400

    Class Player attributes

---
+++ src/main/java/Entities/Player.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Player {
+    // Represents a player in the game
+
+    public String name;
+    public int money;
+    public ArrayList<Property>();
+    public boolean inJail;
+    public int jailCards;
+    public int position;
+    public Map<Integer, Integer> actions;
+
+}
