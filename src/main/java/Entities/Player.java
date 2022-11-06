f68943ef67e7528790256aa96e092da34a28d697
giaphuongd <giaphuongd@gmail.com>
2022-11-06 15:01:24 -0500

    Class Player attributes

--- src/main/java/Entities/Player.java
+++ src/main/java/Entities/Player.java
@@ -1,7 +1,12 @@
 package Entities;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 public class Player {
     // Represents a player in the game
@@ -12,6 +17,73 @@ public class Player {
     public boolean inJail;
     public int jailCards;
     public int position;
-    public Map<Integer, Integer> actions;
+    // public Map<Integer, Integer> actions;
+
+    public Player(String name){
+        this.name = name;
+        this.money = 1500;
+        this.properties = new ArrayList<Property>();
+        this.inJail = false;
+        this.jailCards = 0;
+        this.position = 0;
+    }
+
+    public String trade(Player tradee, int money, ArrayList<Property> properties, int jailcards){
+        if (money > this.money) {
+            return "Inadequate amount of money";
+        } else {
+            this.money -= money;
+            tradee.money += money;
+            tradee.properties.addAll(properties);
+            this.properties.removeAll(properties);
+            this.jailCards -= jailcards;
+            tradee.jailCards += jailCards;
+            return "Trade successful";
+        }
+    }
+
+    public void changeJailStatus(Player player){
+        this.inJail = !this.inJail;
+    }
+
+    // public getPossibleActions(Player player){}
+
+    // public auction(Property property){}
+
+    public String steal(Player victim){
+        double success = Math.random();
+        if (success <= 0.3) {
+            this.money += 100;
+            victim.money -= 100;
+            return this.name + " stole money from " + victim.name;
+        } else {
+            System.out.println("The police are looking for " + this.name);
+            double jail = Math.random();
+            if (jail <= 0.6) {
+                this.inJail = true;
+                return this.name + " is put in jail";
+            } else {
+                return this.name + " escaped from the police";
+            }
+        }
+    }
+
+    public void save() throws IOException {
+        File savefile = new File("Save.txt");
+
+        FileWriter fw = new FileWriter(savefile);
+
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(name);
+        pw.println(money);
+        pw.println(inJail);
+        pw.println(jailCards);
+        pw.println(position);
+        pw.println(properties);
+        pw.println();
+        pw.close();
+    }
+
+    // public void load(){}
 
 }
