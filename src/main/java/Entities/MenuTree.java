5dc29e9a1a36b2a78a53abd005b0f53ae90e1995
kRichard32 <richard.kha@mail.utoronto.ca>
2022-11-09 16:17:33 -0500

    Changed MenuTree, created OutputTree and its tests.

--- src/main/java/Entities/MenuTree.java
+++ src/main/java/Entities/MenuTree.java
@@ -3,19 +3,15 @@ package Entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class MenuTree<T>{
-    private List<MenuTree<T>> children;
-    private MenuTree<T> parent;
-    private String id;
+public abstract class MenuTree{
 
-    public MenuTree(String id){
-        this.children = new ArrayList<>();
-        this.parent = null;
-        this.id = id;
-    }
-    abstract MenuTree<T> getParent();
-    abstract List<MenuTree<T>> getChildren();
-    abstract String getId();
-    abstract void setParent(MenuTree<T> parent);
-    abstract void addChild(MenuTree<T> child);
+    protected List<MenuTree> children = new ArrayList<>();
+    protected MenuTree parent;
+    protected String id;
+
+    public abstract MenuTree getParent();
+    public abstract List<MenuTree> getChildren();
+    public abstract String getId();
+    public abstract void setParent(MenuTree parent);
+    public abstract void addChild(MenuTree child);
 }
