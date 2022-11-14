e3251cf7de068b57fc8d918351b784d10d61a8a5
Cindywwang <xinyun.wang@mail.utoronto.ca>
2022-11-14 13:27:29 -0500

    changed addHouses, mortgage, deleted load method

--- src/main/java/Entities/Player.java
+++ src/main/java/Entities/Player.java
@@ -20,7 +20,7 @@ public class Player {
     public int position;
     // public Map<Integer, Integer> actions;
 
-    public Player(String name){
+    public Player(String name) {
         this.name = name;
         this.money = 1500;
         this.properties = new ArrayList<Property>();
@@ -29,7 +29,7 @@ public class Player {
         this.position = 0;
     }
 
-    public String trade(Player tradee, int money, ArrayList<Property> properties, int jailcards){
+    public String trade(Player tradee, int money, ArrayList<Property> properties, int jailcards) {
         if (money > this.money) {
             return "Inadequate amount of money";
         } else {
@@ -43,7 +43,7 @@ public class Player {
         }
     }
 
-    public void changeJailStatus(Player player){
+    public void changeJailStatus(Player player) {
         this.inJail = !this.inJail;
     }
 
@@ -51,7 +51,7 @@ public class Player {
 
     // public auction(Property property){}
 
-    public String steal(Player victim){
+    public String steal(Player victim) {
         double success = Math.random();
         if (success <= 0.3) {
             this.money += 100;
@@ -88,26 +88,27 @@ public class Player {
     // public void load(){}
 
 
-    public void move(int step){
+    public void move(int step) {
         position += step;
-        if (step > 40){
-            position -= 40;}
+        if (position > 39) {
+            position -= 40;
+        }
     }
 
     public String rollDice() {
         int max = 6;
         int min = 1;
-        int a =  (int) Math.floor(Math.random() * (max - min + 1) + min);
+        int a = (int) Math.floor(Math.random() * (max - min + 1) + min);
         int b = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        if (this.inJail){
-            if(a == b){
+        if (this.inJail) {
+            if (a == b) {
                 this.inJail = false;
                 this.move(a + b);
                 return (a + "\n" + b);
             }
-        }
-        else {
-            if (a == b){
+        } else {
+            if (a == b) {
+                this.move(a + b);
                 this.rollDice();
             } else {
                 this.move(a + b);
@@ -118,60 +119,50 @@ public class Player {
         return (a + "\n" + b);
     }
 
-    public void buildHouse(Property property){
-            properties.add(property);
+    public void buildHouse(Property property, int houses) {
+        if (properties.contains(property)) {
+            property.addHouse(houses);
         }
-
-    public void changeMoney(int change){
-        money += change;}
-
-    public void pay(int money){
-        this.money -= money;}
-
-    public void pay(Player player, int money){
-        this.money -= money;
-        player.money += money;}
-
-    public void mortgage(Property property){
-        this.properties.remove(property);
-//        this.money += property.getMortgageValue();
     }
+
+    public void changeMoney(int change) {
+        money += change;
+    }
+
+    public void pay(int money) {
+        this.money -= money;
+    }
+
+    public void pay(Player player, int money) {
+        this.money -= money;
+        player.money += money;
+    }
+
+    public void mortgage(Property property) {
+        this.properties.remove(property);
+        this.money += property.getMortgageValue();
+    }
+
     public void load(File pw) {
-        try{
-        BufferedReader br = new BufferedReader(new FileReader(pw));
-        String line;
-        ArrayList all = new ArrayList<ArrayList>();
-        while ((line = br.readLine()) != null){
-            ArrayList player = new ArrayList<String>();
-            player.add(line);
-            if(line == "\n"){
-                all.add(player);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pw));
+            String line;
+            ArrayList all = new ArrayList<ArrayList>();
+            while ((line = br.readLine()) != null) {
+                ArrayList player = new ArrayList<String>();
+                player.add(line);
+                if (line == "\n") {
+                    all.add(player);
+                }
             }
-        }
-        }
-        catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             throw new RuntimeException(ex);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
 
     }
-    private void createPlayer(ArrayList<ArrayList> All){
-        for(ArrayList player: All){
-            String name = (String) player.get(0);
-            Player p = new Player(name);
-            p.money = (int)player.get(1);
-            p.inJail = (boolean) player.get(2);
-            p.jailCards = (int) player.get(3);
-            p.position = (int) player.get(4);
-            int length = player.size();
-            for(int i = 5;i < length;i ++){
-                p.properties.add((Property)player.get(i));
-            }
 
 
-        }
-    }
-
-    }
+}
 
