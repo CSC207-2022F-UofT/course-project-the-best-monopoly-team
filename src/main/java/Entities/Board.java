3aea4beb79a84f483df111a7e193661ab9a50f0b
susannah-c <susannah.cao@mail.utoronto.ca>
2022-11-16 16:45:47 -0500

    Made some changes with Board the type of instance variables, players and cells. Create players with type List of Player and cells with type List of Cells. Modify getPlayerCell return statement to match with class method of List.

--- src/main/java/Entities/Board.java
+++ src/main/java/Entities/Board.java
@@ -1,16 +1,17 @@
 package Entities;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board{
 
-    Player[] players;
-    Cell[] cells;
+    List<Player> players;
+    List<Cell> cells;
     Map <Player, Integer> playerPositions = new HashMap<Player, Integer>();
     Property[] properties;
 
-    public Board(Player[] players, Cell[] cells, Object[][] propertyData){
+    public Board(List<Player> players, List<Cell> cells, Object[][] propertyData){
         this.players = players;
         this.cells = cells;
         for (Player player : players) {
@@ -22,43 +23,26 @@ public class Board{
             String name = (String) data[0];
             String colour = (String) data[1];
             int cost = (int) data[2];
-            int houseCost = (int) data[3];
-            int rent = (int) data[4];
-            int rent1H = (int) data[5];
-            int rent2H = (int) data[6];
-            int rent3H = (int) data[7];
-            int rent4H = (int) data[8];
-            int rentHotel = (int) data[9];
-            Player owner = null;
-            if ((int) data[10] != 0) {
-                owner = players[(int) data[10]];
-            }
-            int mortgageValue = (int) data[11];
-            int houses = (int) data[12];
-            boolean mortgaged = false;
-            String mortgageString = (String) data[13];
-            if (mortgageString.equals("true")) {
-                mortgaged = true;
-            }
-            properties[i] = new Property(name, colour, cost, houseCost, rent, rent1H,
-                                        rent2H, rent3H, rent4H, rentHotel, owner,
-                                        mortgageValue, houses, mortgaged);
+            int rent = (int) data[3];
+            int mortgage = (int) data[4];
+            int houseCost = (int) data[5];
+            properties[i] = new Property(name, colour, cost, rent, mortgage, houseCost);
         }
         this.properties = properties;
     }
 
-    public Player[] getPlayers(){
+    public List<Player> getPlayers(){
         return players;
     }
 
-    public Cell[] getCells(){return cells;}
+    public List<Cell> getCells(){return cells;}
 
     public Map<Player, Integer> getPlayerPositions(){return playerPositions;}
 
     public Property[] getProperties(){return properties;}
 
     public Cell getPlayerCell(Player player){
-        return this.cells[this.playerPositions.get(player)];
+        return this.cells.get(this.playerPositions.get(player));
     }
 
     public void updatePlayerPosition(Player player){
