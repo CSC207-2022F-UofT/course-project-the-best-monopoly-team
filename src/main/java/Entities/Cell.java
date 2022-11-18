61062dabefee776f12f2525b88953ec6a7dcad2d
Yue Fung Lee <yuefungalt@gmail.com>
2022-11-18 16:13:39 -0500

    delted redundant cell method

--- src/main/java/Entities/Cell.java
+++ src/main/java/Entities/Cell.java
@@ -1,9 +1,7 @@
 package Entities;
 
 abstract class Cell {
-    public String performAction(){
-        return "";
-    }
+
     public abstract String performAction(Player player, Board board);
 
 }
