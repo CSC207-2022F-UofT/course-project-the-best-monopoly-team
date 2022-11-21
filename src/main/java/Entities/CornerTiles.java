077fba0f579851ea6043582752deb2c2c2627a3a
kRichard32 <richard.kha@mail.utoronto.ca>
2022-11-20 22:24:27 -0500

    Fixed cases in CornerTiles class

--- src/main/java/Entities/CornerTiles.java
+++ src/main/java/Entities/CornerTiles.java
@@ -13,18 +13,22 @@ public class CornerTiles extends Cell{
         switch (this.type) {
             case "Go":
                 returnMessage = "Passed Go! Collected 200";
+                break;
             case "jail":
                 if (currentPlayer.isInJail()) {
                     returnMessage = "You are still in Jail";
                 } else {
                     returnMessage = "You're visiting Jail";
                 }
+                break;
             case "freeParking":
                 returnMessage = "You landed on Free Parking!";
+                break;
             case "goToJail":
                 currentPlayer.setPosition(11);
                 currentPlayer.changeJailStatus();
                 returnMessage = "Go to Jail!";
+                break;
         }
         return returnMessage;
     }
