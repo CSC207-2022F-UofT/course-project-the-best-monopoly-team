94019e5740a7cc50da8aa06a30cb43a8c5886779
Cindywwang <xinyun.wang@mail.utoronto.ca>
2022-11-21 21:46:57 -0500

    wrote javadocs for Cell and CornerTiles and created tests for CornerTiles.

--- src/main/java/Entities/CornerTiles.java
+++ src/main/java/Entities/CornerTiles.java
@@ -3,13 +3,26 @@ package Entities;
 public class CornerTiles extends Cell{
     private String type;
 
+    /**
+     * The constructor for CornerTiles.
+     * @param type The type of the Player/
+     */
     public CornerTiles(String type) {
         this.type = type;
     }
+
+    /**
+     * Take the player and the board and decide which action
+     * should be performed on the cornerTiles according to its
+     * type.
+     * @param currentPlayer
+     * @param board
+     * @return The message associated with the action.
+     */
     @Override
     public String performAction(Player currentPlayer, Board board) {
         //TODO: check this.type and perform corresponding action
-        String returnMessage = new String();
+        String returnMessage = "";
         switch (this.type) {
             case "Go":
                 returnMessage = "Passed Go! Collected 200";
