46cbcb80ab6ef9d807947215c6520315943ff120
Yue Fung Lee <yuefungalt@gmail.com>
2022-11-25 14:42:02 -0500

    Changed cell

--- src/main/java/Entities/Cell.java
+++ src/main/java/Entities/Cell.java
@@ -2,5 +2,6 @@ package Entities;
 
 public abstract class Cell {
     public abstract String performAction(Player player, Board board);
+    public abstract String getType(Player player, Board board);
 }
 
