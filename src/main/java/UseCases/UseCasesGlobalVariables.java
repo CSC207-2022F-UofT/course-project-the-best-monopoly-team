37ac61417f6c8047207415bb2ebbb7a0510d1894
giaphuongd <giaphuongd@gmail.com>
2022-11-27 13:21:29 -0500

    Refactored Player class Added PlayerLogic Added classes to store global variables for Entities and Use Cases

---
+++ src/main/java/UseCases/UseCasesGlobalVariables.java
@@ -0,0 +1,9 @@
+package UseCases;
+
+public class UseCasesGlobalVariables {
+    static final int BROWN_DARKBLUE_SETSIZE = 2;
+    static final int PROPERTY_SETSIZE = 3;
+    static final double STEAL_CHANCE = 0.3;
+    static final double STEAL_JAIL_CHANCE = 0.6;
+    static final int STEAL_MONEY = 100;
+}
