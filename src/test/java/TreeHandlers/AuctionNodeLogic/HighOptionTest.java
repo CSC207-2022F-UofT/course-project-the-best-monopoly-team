3e4985e432ae6d76f89b2689130b82696e8d094b
derrcodes <derrick281003@gmail,.com>
2022-12-04 23:34:31 -0500

    Implemented the HighOptionTest test

--- src/test/java/TreeHandlers/AuctionNodeLogic/HighOptionTest.java
+++ src/test/java/TreeHandlers/AuctionNodeLogic/HighOptionTest.java
@@ -1,12 +1,34 @@
 package TreeHandlers.AuctionNodeLogic;
 
+import Entities.*;
+import Interactors.GameLogic;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HighOptionTest {
 
     @Test
     public void testHighOptionCreateState(){
-
+        Player playerOne = new Player("Player One");
+        Player playerTwo = new Player("Player Two");
+        List<Player> players = new ArrayList<>();
+        players.add(playerOne);
+        Property test_property1 = new Property("Property One", "Blue", 100, 100, new int[5],
+                playerOne, 50, 0, false);
+        Property test_property2 = new Property("Property One", "Blue", 100, 100, new int[5],
+                null, 50, 0, false);
+        playerOne.addProperty(test_property1);
+        List<Cell> cells = new ArrayList<>();
+        cells.add(test_property1);
+        cells.add(test_property2);
+        Board board = new Board(players, cells);
+        GameLogic gameLogic = new GameLogic(playerOne, board);
+        HighOption highOption = new HighOption();
+        State actual = highOption.create_state(0);
+        Assertions.assertEquals(actual.getId(), "Main Tree Parent Node");
     }
 
 }
