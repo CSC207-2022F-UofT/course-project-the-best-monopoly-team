1d1649415192d09e589dff5d92258ed98ae696f1
derrcodes <derrick281003@gmail,.com>
2022-12-04 22:56:30 -0500

    Added PickItemOpTest

---
+++ src/test/java/TreeHandlers/MainTreeLogic/ManagePropertyTest.java
@@ -0,0 +1,52 @@
+package TreeHandlers.MainTreeLogic;
+
+import Entities.*;
+import Interactors.GameLogic;
+import TreeHandlers.MainTreeNodeLogic.ManageProperty;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagePropertyTest {
+
+    @Test
+    public void testManagePropertyCreateStateNoProperties(){
+        Player playerOne = new Player("Player One");
+        List<Player> players = new ArrayList<>();
+        players.add(playerOne);
+        Property test_property = new Property("Name", "Blue", 100, 100, new int[5],
+                null, 50, 0, false);
+        List<Cell> cells = new ArrayList<>();
+        cells.add(test_property);
+        Board board = new Board(players, cells);
+        GameLogic gameLogic = new GameLogic(playerOne, board);
+        ManageProperty manageProperty = new ManageProperty();
+        State actual = manageProperty.create_state(0);
+        Assertions.assertEquals(actual.getId(), "User Has No Properties (Manage Properties)");
+        Assertions.assertEquals(actual.isBackEnable(), false);
+    }
+
+    @Test
+    public void testManagePropertyCreateStateWithProperties(){
+        Player playerOne = new Player("Player One");
+        List<Player> players = new ArrayList<>();
+        players.add(playerOne);
+        Property test_property = new Property("Name", "Blue", 100, 100, new int[5],
+                playerOne, 50, 0, false);
+        playerOne.addProperty(test_property);
+        List<Cell> cells = new ArrayList<>();
+        cells.add(test_property);
+        Board board = new Board(players, cells);
+        GameLogic gameLogic = new GameLogic(playerOne, board);
+        ManageProperty manageProperty = new ManageProperty();
+        State actual = manageProperty.create_state(0);
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("Name");
+        Assertions.assertEquals(actual.getId(), "Manage Property");
+        Assertions.assertEquals(actual.isBackEnable(), true);
+        Assertions.assertEquals(actual.getOptions(), options);
+    }
+
+}
