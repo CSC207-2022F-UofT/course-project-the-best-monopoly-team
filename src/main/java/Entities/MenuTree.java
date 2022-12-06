8ec1c4a0b55bd42fa44e58894c06f7b1f490c8c3
derrcodes <derrick281003@gmail,.com>
2022-12-06 14:30:07 -0500

    Added JavaDoc to the NodeLogic interactor and the MenuTree abstract class.

--- src/main/java/Entities/MenuTree.java
+++ src/main/java/Entities/MenuTree.java
@@ -3,6 +3,9 @@ package Entities;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An abstract class representing a tree representing the flow of the game.
+ */
 public abstract class MenuTree{
 
     protected List<MenuTree> children = new ArrayList<>();
