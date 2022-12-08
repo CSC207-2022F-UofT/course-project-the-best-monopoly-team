9c866b5a08fcc114ca1778d6c5d3e38c4a60116a
Yue Fung Lee <yuefungalt@gmail.com>
2022-12-07 20:46:52 -0500

    Added ending game description string based on the two possible end game states

--- src/main/java/TreeHandlers/MainTreeNodeLogic/FinishGameUseCase.java
+++ src/main/java/TreeHandlers/MainTreeNodeLogic/FinishGameUseCase.java
@@ -2,6 +2,11 @@ package TreeHandlers.MainTreeNodeLogic;
 
 import Entities.State;
 import Interface.NodeLogic;
+import Entities.Player;
+import Entities.Board;
+
+import java.util.List;
+
 
 public class FinishGameUseCase extends MainTreeNodeLogic implements NodeLogic {
     public FinishGameUseCase(){
@@ -13,6 +18,19 @@ public class FinishGameUseCase extends MainTreeNodeLogic implements NodeLogic {
         State currentState = new State();
         currentState.setId("Game Complete");
         currentState.addOptions("Ok");
+
+        // The case that there is only one player in the player array
+
+        // handles the case where the max turns are met
+        if (mainStates[3] >= mainStates[4]) {
+            currentState.setDescription("Max turn reaches, game over, no one wins. RIP.");
+        } else if (getPlayers().size() == 1) {
+            List<Player> players = getPlayers();
+            Player winningPlayer = players.get(0);
+            String winnningString = winningPlayer.getName() + " won the game!!!";
+            currentState.setDescription(winnningString);
+        }
+
         mainStates[0] = 1;
         return currentState;
 
